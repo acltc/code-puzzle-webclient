5b8f7ad1c21fc8ef709b414a842ba1c9b47d555a
Harper Essenfeld <harper.essenfeld@gmail.com>
2014-07-27 14:28:16 -0500

    working on search functionality

--- app/controllers/puzzles_controller.rb
+++ app/controllers/puzzles_controller.rb
@@ -1,11 +1,12 @@
 class PuzzlesController < ApplicationController
 
+  def home
+  end
 
   def show
       @puzzle = Puzzle.find(params[:id])
     end
 
-  end
   def index
     @puzzles = Unirest.get("http://localhost:3000/api/v1/puzzles.json", :headers => {"Accept" => "application/json"}).body
     
