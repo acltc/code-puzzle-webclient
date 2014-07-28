fcd5d77a4f9449612737c144b84a70fe684af913
Chelsea <chelsea.dommert@gmail.com>
2014-07-28 16:42:46 -0500

    working on solution

--- app/models/puzzle.rb
+++ app/models/puzzle.rb
@@ -1,11 +1,16 @@
 class Puzzle
-  attr_accessor :id, :name, :instructions, :solution
+  attr_accessor :id, :name, :instructions, :solution. :code_solution
 
   def initialize(attribute_hash)
     @id = attribute_hash["id"]
     @name = attribute_hash["name"]
     @instructions = attribute_hash["instructions"]
+    @code_solution = ""
     end  
+
+  def solution_correct?
+    if code_solution
+  end
 
  def self.find(id)
     hash = Unirest.get("http://localhost:3000/api/v1/puzzles/#{id}.json", :headers => {"Accept"=>"application/json"}).body
