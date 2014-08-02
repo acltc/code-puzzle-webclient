d4334dec20f4dae876cd78552f1f6e0df52a760d
Chelsea <chelsea.dommert@gmail.com>
2014-08-02 15:42:57 -0500

    Tracks if user has solved each puzzle, and tracks total number of puzzles user has solved

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def conquer_count
+    TrackSuccess.where(:user_id => :id, :success => true).count
+  end
 end
